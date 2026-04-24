2333df173f380fee103204a78b8f171f627681a1
PnjDark <patrickjnarmaye@gmail.com>
2026-04-24 13:11:08 +0100

    `Updated Gradle build files and pubspec.lock to reflect changes in dependencies and syntax`

--- Projects/mealsnap/android/build.gradle.kts
+++ Projects/mealsnap/android/build.gradle.kts
@@ -5,16 +5,6 @@ allprojects {
     }
 }
 
-val newBuildDir: Directory =
-    rootProject.layout.buildDirectory
-        .dir("../../build")
-        .get()
-rootProject.layout.buildDirectory.value(newBuildDir)
-
-subprojects {
-    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
-    project.layout.buildDirectory.value(newSubprojectBuildDir)
-}
 subprojects {
     project.evaluationDependsOn(":app")
 }
@@ -25,10 +15,10 @@ tasks.register<Delete>("clean") {
 
 buildscript {
     repositories {
-      google()
-      mavenCentral()
+        google()
+        mavenCentral()
     }
     dependencies {
-      classpath("com.google.gms:google-services:4.4.2")
+        classpath("com.google.gms:google-services:4.4.2")
     }
-  }
+}
