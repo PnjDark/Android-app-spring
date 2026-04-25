83a2617e32d1e8cdad19dfb78fe49fca12d61c2e
patrickjnarmaye <patrickjnarmaye@gmail.com>
2026-04-25 03:57:15 +0000

    buils edit

--- Projects/mealsnap/test/mock.dart
+++ Projects/mealsnap/test/mock.dart
@@ -1,11 +1,4 @@
 import 'package:firebase_auth_mocks/firebase_auth_mocks.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/widgets.dart';
-
-Future<T> setupFirebase<T>(Future<T> Function() callback) async {
-  await Firebase.initializeApp();
-  return callback();
-}
 
 void setupFirebaseAuthMocks() {
   final auth = MockFirebaseAuth();
